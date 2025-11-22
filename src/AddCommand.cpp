01d12dec6a7f7996c2657c654ed4f4f92cd8167d
Ayelet <ayeletb123@gmail.com>
2025-11-22 22:12:38 +0200

    Fixing input/output and tests - it now works!

--- src/AddCommand.cpp
+++ src/AddCommand.cpp
@@ -1,7 +1,6 @@
 #include "ICommand.h"
 #include <string>
 #include "ICompress.h"
-#include "IOutput.h"
 
 //The class AddCommand inherits from the ICommand interface.
 //It is responsible for adding new files with compress content to the system.
