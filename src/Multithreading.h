b890f55b095b9855b68ce7c44329b86577a5ad66
ShirYona <sycohen2002@gmail.com>
2025-12-03 17:51:30 +0200

    Multithreading.h commit

---
+++ src/Multithreading.h
@@ -0,0 +1,24 @@
+#ifndef MULTITHREADING_H
+#define MULTITHREADING_H
+
+#include "IThreads.h"
+#include <thread>
+#include <vector>
+#include <functional>
+#include <atomic>
+#include <mutex>
+
+class Multithreading : public IThreads {
+private:
+std::vector[std::thread](std::thread) threads; // Vector to store running threads
+std::mutex mtx;                    // Protect threads vector
+public:
+Multithreading();
+~Multithreading();
+
+// Launch a new task in a separate thread
+void launch(std::function<void()> task) override;
+
+};
+
+#endif // MULTITHREADING_H
