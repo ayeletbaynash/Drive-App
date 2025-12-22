0f7fffe473906761432254501cbe72f4e0b15793
Ayelet <ayeletb123@gmail.com>
2025-12-22 21:09:57 +0200

    fix rout

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/users'); 
 
 // Registration: Submit new user details
-router.post('/users', userController.registerUser);
+router.post('/', userController.registerUser);
 
 // Get user information: Retrieve by ID in the path
-router.get('/users/:id', userController.getUserProfile);
+router.get('/:id', userController.getUserProfile);
 
 module.exports = router;
