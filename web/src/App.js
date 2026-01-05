7d977b10c3f602403b025494c038122f6bd9135f
Ayelet <ayeletb123@gmail.com>
2026-01-06 00:19:13 +0200

    register and login with theme

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './pages/registration'; 
+import Login from './pages/login';
 
 function App() {
   return (
     <div>
+      <Router>
+        <Routes>
+          <Route path="/registration" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
     </div>
+    
   );
 }
 
