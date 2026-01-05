aecbaf74cdf99ec9f6d5d49dfe75089f20bcd7a7
Maayan Tsuriely <tsuriely.m@gmail.com>
2026-01-06 00:42:43 +0200

    side bar+start with main content

---
+++ web/src/components/files/FileViewList.jsx
@@ -0,0 +1,28 @@
+import FolderItem from './FolderItem';
+import FileItem from './FileItem';
+
+
+
+const FileViewList = ({items, onRefresh}) =>{
+    const folders = items.filter(item => item.type === 'folder');
+    const files = items.filter(item => item.type === 'file');
+    return(
+        <div>
+            <h2>folders</h2>
+            <div>
+                {folders.map(f => (
+                    <FolderItem folder={f} onRefresh={onRefresh} />
+                ))}
+            </div>
+            <h2>files</h2>
+            <div>
+                {files.map(f => (
+                    <FileItem file={f} onRefresh={onRefresh} />
+                ))}
+            </div>
+        </div>
+    );
+
+};
+
+export default FileViewList;
